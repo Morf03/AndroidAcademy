797a202bba6e5552441cd203530f8369ef02c3e3
Ilya Markelov <markell0308@mail.ru>
2018-10-08 23:21:30 +0300

    add email app

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.user.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 Button button;
 TextView textView5;
 EditText editText;
+String[] emailTo;
+String emailSubject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,14 @@ EditText editText;
 
         button = (Button)  findViewById(R.id.button);
         editText =(EditText) findViewById(R.id.editText);
+        emailTo = new String[]{getString(R.string.email_to_me)};
+        emailSubject = getString(R.string.email_subject);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                openSecondActivity();
+                composeEmail(emailTo, emailSubject,editText.getText().toString());
             }
         });
 
@@ -37,4 +43,18 @@ EditText editText;
         startActivity(secondActivityIntent);
     }
 
-}
+    public void composeEmail(String[] addresses, String subject, String emailBody) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_EMAIL, emailBody);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }else {
+            Toast.makeText(this, "No Email app found", Toast.LENGTH_LONG).show();
+        }
+        }
+    }
+
+
